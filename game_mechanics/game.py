ac0e1009042267c7f1a115437ae454a2908caed0
Galit Dotan <galitdotan99@gmail.com>
2023-12-01 20:03:30 +0200

    Moved player order to Game State

--- game_mechanics/game.py
+++ game_mechanics/game.py
@@ -1,4 +1,3 @@
-from random import shuffle
 from typing import Optional
 
 from consts import V_CARDS_PER_PLAYERS, CURSES_CARDS_PER_PLAYER, FIRST_GAME_CARDS, STANDARD_CARDS, DEFAULT_PILE_SIZE, \
@@ -12,11 +11,14 @@ from game_mechanics.screens.score_board import ScoreBoard
 from game_mechanics.states.game_state import GameState
 
 
-def _generate_supply_piles(supply_card_types: tuple, supply_pile_sizes: Optional[tuple[int, ...]] = None) -> list[
-    SupplyPile]:  # TODO: change input to list[tuple(Card, int]]
+# TODO: change input to list[tuple(Card, int]]
+def _generate_supply_piles(supply_card_types: tuple,
+                           supply_pile_sizes: Optional[tuple[int, ...]] = None) -> list[SupplyPile]:
     """
     Receives a list of card types and the size of each pile.
-    :return: A list of supply piles.
+
+    Returns:
+        A list of supply piles.
     """
     supply_pile_sizes = supply_pile_sizes if supply_pile_sizes else [DEFAULT_PILE_SIZE for _ in
                                                                      range(len(supply_card_types))]
@@ -40,18 +42,8 @@ class Game:
         self._start_cards = start_cards
 
         self.players: dict[str, Player] = players
-        self._play_order: list[Player] = list(self.players.values())
-        shuffle(self._play_order)
 
-        self.player_index = 0
-
-        num_v_cards = V_CARDS_PER_PLAYERS[self._num_players]
-        num_curses = CURSES_CARDS_PER_PLAYER[self._num_players]
-        sizes = (num_v_cards, num_v_cards, num_v_cards, num_curses, 30, 40, 60)
-        kingdom_piles = _generate_supply_piles(FIRST_GAME_CARDS)  # TODO: allow other cards
-        standard_piles = _generate_supply_piles(STANDARD_CARDS, sizes)
-
-        self.game_state: GameState = GameState(kingdom_piles, standard_piles)
+        self.game_state: Optional[GameState] = None
 
     def __hash__(self):
         return self.id
@@ -60,8 +52,19 @@ class Game:
         return self.id == other.id
 
     @property
-    def curr_player(self):
-        return self._play_order[self.player_index]
+    def player_list(self):
+        return list(self.players.values())
+
+    def start(self):
+        """
+        Generate the initial game state.
+        """
+        num_v_cards = V_CARDS_PER_PLAYERS[self._num_players]
+        num_curses = CURSES_CARDS_PER_PLAYER[self._num_players]
+        sizes = (num_v_cards, num_v_cards, num_v_cards, num_curses, 30, 40, 60)
+        kingdom_piles = _generate_supply_piles(FIRST_GAME_CARDS)  # TODO: allow other cards
+        standard_piles = _generate_supply_piles(STANDARD_CARDS, sizes)
+        self.game_state = GameState(kingdom_piles, standard_piles, self.players)
 
     def run(self):
         """
@@ -69,42 +72,25 @@ class Game:
         """
         self.status = GameStatus.IN_PROGRESS
         while self.status == GameStatus.IN_PROGRESS:
-            curr_player = self.curr_player
-            opponents = self.get_player_opponents()
+            curr_player = self.game_state.curr_player
+            opponents = self.game_state.get_player_opponents()
 
             turn = Turn(curr_player, opponents, self.game_state)
             turn.play()
-            self._to_next_player()
-
-    def get_player_opponents(self, player: Optional[Player] = None) -> list[Player]:
-        """
-        Get all the opponents of a player.
-        If no player name was supplied - returns the opponents of the current player.
-
-        Params:
-            player_name: the player's name
-
-        Returns:
-            A list of opponents (players).
-        """
-        if not player:
-            player = self._play_order[self.player_index]
-        opponents = self._play_order.copy()
-        opponents.remove(player)
-        return list(opponents)
+            self.game_state.move_to_next_player()
 
     def get_player_view(self, player_name: str):
         """
         Get current board view from the PoV of the given player.
         """
         player = self.players[player_name]
-        opponents = self.get_player_opponents(player)
+        opponents = self.game_state.get_player_opponents(player)
         if self.status == GameStatus.INITIATED:
             return OpeningMessage(player, opponents)
         elif self.status == GameStatus.IN_PROGRESS:
             return player.get_board_view()
         elif self.status == GameStatus.FINISHED:
-            return ScoreBoard(self._play_order)
+            return ScoreBoard(self.player_list)
         raise AttributeError(f'Unknown status {self.status}')
 
     def game_over(self, finishing_piles: tuple[str] = DEFAULT_FINISH_PILES) -> bool:
@@ -122,9 +108,3 @@ class Game:
             if pile in empty_pile_names:
                 return True
         return False
-
-    def _to_next_player(self):
-        """
-        Update next player index.
-        """
-        self.player_index = (self.player_index + 1) % self._num_players
--- game_mechanics/states/game_state.py
+++ game_mechanics/states/game_state.py
@@ -1,5 +1,9 @@
+from random import shuffle
+from typing import Optional
+
 from game_mechanics.card_structures.supply_pile import SupplyPile
 from game_mechanics.card_structures.trash import Trash
+from game_mechanics.player.player import Player
 from game_mechanics.states.base_state import BasePublicState
 from game_mechanics.supply import Supply
 
@@ -12,6 +16,48 @@ class GameState(BasePublicState):
         3. Public mats
     """
 
-    def __init__(self, kingdom_piles: list[SupplyPile], standard_piles: list[SupplyPile]):
+    def __init__(self, kingdom_piles: list[SupplyPile], standard_piles: list[SupplyPile], players: dict[str, Player]):
+        """
+        Establish player order.
+        Initiate all the card structures that are part of the game.
+
+        Params:
+            kingdom_piles: The kingdom piles.
+            standard_piles: The other piles.
+            players: All the players in the game.
+        """
         self.supply = Supply(kingdom_piles, standard_piles)
         self.trash = Trash(name="Trash")
+
+        self._play_order: list[Player] = list(players.values())
+        shuffle(self._play_order)
+
+        self.player_index = 0
+        self._num_players = len(players)
+
+    @property
+    def curr_player(self):
+        return self._play_order[self.player_index]
+
+    def get_player_opponents(self, player: Optional[Player] = None) -> list[Player]:
+        """
+        Get all the opponents of a player.
+        If no player name was supplied - returns the opponents of the current player.
+
+        Params:
+            player_name: the player's name
+
+        Returns:
+            A list of opponents (players).
+        """
+        if not player:
+            player = self.curr_player
+        opponents = self.get_player_opponents(player)
+        opponents.remove(player)
+        return list(opponents)
+
+    def move_to_next_player(self):
+        """
+        Update next player index.
+        """
+        self.player_index = (self.player_index + 1) % self._num_players
