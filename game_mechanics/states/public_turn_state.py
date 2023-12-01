46e0d476e3d37fd25579b9d98f86e8c8b0940986
Galit Dotan <galitdotan99@gmail.com>
2023-12-01 17:15:55 +0200

    Added public turn state

---
+++ game_mechanics/states/public_turn_state.py
@@ -0,0 +1,8 @@
+from game_mechanics.states.base_state import BaseState
+
+
+class PublicTurnState(BaseState):
+    """
+    The current state of all public elements that would be discarded/ rebooted at the end of the turn.
+    """
+    pass
