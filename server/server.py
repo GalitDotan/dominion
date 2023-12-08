497c564ae41bcc06248df394afff3f29e492ad3b
Galit Dotan <galitdotan99@gmail.com>
2023-12-08 11:07:39 +0200

    Renamed param

--- server/server.py
+++ server/server.py
@@ -13,7 +13,7 @@ root = 'static/templates'
 
 class ConnectionManager:
     def __init__(self):
-        self.active_connections: list[WebSocket] = []
+        self.active_connections: list[WebSocket] = []  # TODO: each client should have a game id
 
     async def connect(self, websocket: WebSocket):
         await websocket.accept()
@@ -40,17 +40,17 @@ async def get():
     return HTMLResponse(content=data, media_type="text/html")
 
 
-@app.websocket("/ws/{client_id}")
-async def websocket_endpoint(websocket: WebSocket, client_id: int):
+@app.websocket('/ws/{client_name}')
+async def websocket_endpoint(websocket: WebSocket, client_name: str):
     await manager.connect(websocket)
     try:
         while True:
             data = await websocket.receive_text()
             await manager.send_personal_message(f"You wrote: {data}", websocket)
-            await manager.broadcast(f"Client #{client_id} says: {data}")
+            await manager.broadcast(f"Client #{client_name} says: {data}")
     except WebSocketDisconnect:
         manager.disconnect(websocket)
-        await manager.broadcast(f"Client #{client_id} left the chat")
+        await manager.broadcast(f"Client #{client_name} left the chat")
 
 
 if __name__ == "__main__":
