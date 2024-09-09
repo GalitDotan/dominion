83f1400c6fab285af40be5fbb2c9622742b09156
Galit Dotan <galitdotan99@gmail.com>
2024-09-09 09:11:00 +0300

    More names

--- utils/name_generator.py
+++ utils/name_generator.py
@@ -4,13 +4,42 @@ from wonderwords import RandomWord
 
 RW = RandomWord()
 
-available_names = ['SIRI', 'LIBI', 'KIKI', 'BABA', 'NIMI', 'LILY']
+available_names = [
+    'SIRI',  # Apple's voice assistant (and my username in the OG dominion game)
+    'GALA',  # my nickname :)
+    'LIBI',  # no reference
+    'KIKI',  # 'Baba is You' game
+    'BABA',  # 'Baba is You' game
+    'NIMI',  # no reference
+    'LILY',  # different characters
+    'ZAZA',  # no reference
+    'MOJO',  # 'The Powerpuff Girls'
+    'JOJO',  # 'The Powerpuff Girls'
+    'MIMI',  # no reference
+    'NANA',  # no reference
+    'ROBO',  # generic robot name
+    'LUCA',  # Disney's 'Luca'
+    'GAGA',  # famous singer
+    'LOLO',  # no reference
+    'MOKO',  # no reference
+    'DIDI',  # no reference
+    'TOTO',  # 'The Wizard of Oz'
+    'ZAZI',  # no reference
+    'DORA',  # 'Dora the Explorer'
+    'LILO',  # 'Lilo & Stitch'
+    'RICKI',  # different characters
+    'BIMO',  # Adventure Time
+    'TINA',  # different characters
+    'MOMO',  # 'Avatar: The Last Airbender'
+    'ZAZU',  # 'The Lion King'
+]
+
 GAME_NAME_FORMAT = "{host_name}'s {adj} Game"
 
 
 def generate_player_name() -> str:
     """
-    Generate a players name.
+    Generate a player's name.
     """
     name = random.choice(available_names)
     available_names.remove(name)
